d5fd51562a441adac250cdb5248c5e490a259eb7
Chupakabra0 <rootalfa4@gmail.com>
2021-05-06 23:27:26 +0300

    Project init

---
+++ Model/Teacher.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConsoleDBTest.Models {
+    public class Teacher {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name       { get; set; }
+        public string Surname    { get; set; }
+        public string Patronymic { get; set; }
+        public int    CityId     { get; set; }
+        public int    CathedraId { get; set; }
+        public bool   IsActive   { get; set; }
+
+        public virtual ICollection<ClientCard> ClientCards { get; set; }
+
+        public virtual City     City     { get; set; }
+        public virtual Cathedra Cathedra { get; set; }
+    }
+}
---
+++ Model/Worker.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConsoleDBTest.Models {
+    public class Worker {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name       { get; set; }
+        public string Surname    { get; set; }
+        public string Patronymic { get; set; }
+        public bool   IsActive   { get; set; }
+
+        public virtual ICollection<LibraryTransaction> Libraries { get; set; }
+    }
+}
