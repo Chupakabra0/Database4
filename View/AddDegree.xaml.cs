5ec55676a6d3f8d7fdcea2657fa3003f7e97e7b1
Chupakabra0 <rootalfa4@gmail.com>
2021-05-07 20:15:47 +0300

    AddDegree view is ready

---
+++ View/AddDegree.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Database4.View {
+    /// <summary>
+    /// Логика взаимодействия для AddDegree.xaml
+    /// </summary>
+    public partial class AddDegree : Window {
+        public AddDegree() {
+            InitializeComponent();
+        }
+    }
+}
