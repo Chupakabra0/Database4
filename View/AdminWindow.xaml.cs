96ef35fb0bf458ffedd15b985df900d6a1009899
Chupakabra0 <rootalfa4@gmail.com>
2021-05-07 20:10:54 +0300

    AdimnWindow renaming

--- View/AdminWindow.xaml.cs
+++ View/AdminWindow.xaml.cs
@@ -2,10 +2,10 @@
 
 namespace Database4 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for AdminWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window {
-        public MainWindow() {
+    public partial class AdminWindow : Window {
+        public AdminWindow() {
             InitializeComponent();
         }
     }
